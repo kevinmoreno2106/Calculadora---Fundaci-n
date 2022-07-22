1203397bffb92abab2e0aea5b767043d8d20a3d5
Paul <videoblogpaul@gmail.com>
2022-07-22 13:53:19 +0200

    uwu

---
+++ html.js
@@ -0,0 +1,21 @@
+function doble (x){
+ returm 2*x
+}
+
+const doble2 = fuction(x){
+    returm x*2;
+}
+
+const constadoble3 = x => x*2
+
+
+const doble =  x => x*2
+
+function ejecutar (operacion, valor){
+    returm operacion (valor)
+}
+lista =[1,2,3,4]
+lista.forEach(element => {console.log(element)
+    
+});
+
